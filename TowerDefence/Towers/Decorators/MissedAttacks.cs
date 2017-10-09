310ce20029d02e424bd0376a509d514b9f8d4e6d
Baltulionis <Simonas.Baltulionis@emc.com>
2017-10-09 22:45:50 +0300

    Tower decorator

---
+++ TowerDefence/Towers/Decorators/MissedAttacks.cs
@@ -0,0 +1,9 @@
+﻿namespace TowerDefence.Towers.Decorators
+{
+    public class MissedAttacks : TowerDecorator
+    {
+        public MissedAttacks(ITower tower) : base(tower) {
+            Damage = _tower.Damage - 5;
+        }
+    }
+}
---
+++ TowerDefence/Towers/Decorators/TowerDecorator.cs
@@ -0,0 +1,19 @@
+﻿namespace TowerDefence.Towers.Decorators {
+    public class TowerDecorator : ITower {
+        protected readonly ITower _tower;
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Range { get; set; }
+
+        public TowerDecorator(ITower tower) {
+            _tower = tower;
+            Name = tower.Name;
+            Damage = tower.Damage;
+            Range = tower.Range;
+        }
+        
+        public virtual void Attack() {
+            _tower.Attack();
+        }
+    }
+}
