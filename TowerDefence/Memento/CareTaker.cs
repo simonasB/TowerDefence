fd934a4b330f6bd81267161c8ba6c3076453204a
Simonas Baltulionis <baltulionis.simonas@gmail.com>
2017-12-06 21:15:26 +0200

    Memento structure

---
+++ TowerDefence/Memento/CareTaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace TowerDefence.Memento {
+    public class CareTaker {
+        private List<Memento> _statesList;
+
+        public CareTaker() {
+            _statesList = new List<Memento>();
+        }
+
+        public void Add(Memento state) {
+            _statesList.Add(state);
+        }
+
+        public Memento Get(int index) {
+            Memento restoredState = _statesList[index];
+            _statesList.RemoveAt(index);
+            return restoredState;
+        }
+
+        public int Size() {
+            return _statesList.Count;
+        }
+    }
+}
